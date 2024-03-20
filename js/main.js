74cf54622aed66e7723445fc3ec7ca67d2739ec3
Mihuy1 <patrik.hyytiainen@protonmail.com>
2024-03-20 16:44:33 +0200

    Added leaflet map

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var map = L.map('map').setView([60.1695, 24.9354], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
