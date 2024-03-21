01246b1ddf59c7df89bc83ee6fdd4e3b11364d59
Mihuy1 <patrik.hyytiainen@protonmail.com>
2024-03-21 16:21:16 +0200

    Added register and login modal instead of seperate sites.

--- js/main.js
+++ js/main.js
@@ -6,3 +6,29 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+
+
+const modal = document.getElementById('login-modal-id');
+window.onclick = function() {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+const loginButton = document.getElementById('login-button');
+
+loginButton.addEventListener('click', function() {
+        console.log('click');
+        modal.style.display = "block";
+}
+);
+
+const registerButton = document.getElementById('register-button');
+
+registerButton.addEventListener('click', function() {
+        console.log('click');
+        modal.style.display = "block";
+}
+);
